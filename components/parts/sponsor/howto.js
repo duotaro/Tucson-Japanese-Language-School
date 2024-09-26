da4d0d07c0f1094cd7b6adac5f3ca3f8dfab61ce
Taro Yamauchi <yamauchi@eurekastyle.co.jp>
2024-09-26 23:58:44 +0900

    donation page

--- components/parts/sponsor/howto.js
+++ components/parts/sponsor/howto.js
@@ -6,14 +6,15 @@ import LocaleContext from "../../context/localeContext";
 import Link from "next/link";
 import { useLocale } from "../../../utils/locale";
 import Section from "../section";
+import Title from "../text/title";
 import Paragraphs from "../text/paragraphs";
+import { GlobeAsiaAustraliaIcon } from "@heroicons/react/24/outline";
 
 export default function HowToDonate({ howto }) {
   const { locale } = useContext(LocaleContext);
   const { json } = useLocale(locale)
   const isJpn = locale == "ja"
 
-  const [activeTab, setActiveTab] = useState("donations");
 
   let resList = []
   for(let item of howto){
@@ -30,13 +31,14 @@ export default function HowToDonate({ howto }) {
     res.text = isJpn ? item.properties["text"].rich_text : item.properties["text_en"].rich_text
 
     res.tag =  item.properties["tag"].select.name
+    res.image = `/image/components/donation/${res.tag}.jpeg`
     if(isJpn){
         if(item.properties["btn_label"].rich_text[0]){
             res.btnLabel = item.properties["btn_label"].rich_text[0].text.content
             res.btnLink = item.properties["btn_link"].url
         }
     } else {
-        if(item.properties["btn_labe_en"].rich_text[0]){
+        if(item.properties["btn_label_en"].rich_text[0]){
             res.btnLabel = item.properties["btn_label_en"].rich_text[0].text.content
             res.btnLink = item.properties["btn_link"].url
         } 
@@ -50,47 +52,37 @@ export default function HowToDonate({ howto }) {
     
     <Section py="py-8 md:py-12 lg:py-20" bg="bg-gray-100">
     <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
-      <ul
-        className="flex flex-wrap -mb-px text-center justify-center items-center"
-        role="tablist"
-      >
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 w-full">
         {resList.map((item) => {
           return (
-            <li className="me-2" role="presentation">
-              <button
-                className={`min-w-18 md:min-w-32 lg:min-w-36 inline-block p-4 border-b-2 rounded-t-lg ${
-                  activeTab === item.tag
-                    ? "border-blue-500 text-blue-500"
-                    : "hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                }`}
-                onClick={() => setActiveTab(item.tag)}
-                type="button"
-                role="tab"
-                aria-controls={item.tag}
-                aria-selected={activeTab === "donations"}
-              >
-                <span className="text-sm md:text-md lg:text-lg font-bold">{item.title}</span>
-              </button>
-          </li>
-          )
-        })}
-      </ul>
-    </div>
+            <div className="flex flex-col p-5 lg:flex-row">
+                {/* <div className="text-center hidden lg:block">
+                    <img className="h-80 w-80 sm:w-[14rem] sm:h-[14rem] flex-shrink-0 object-cover rounded-full" src="https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80" alt="" />
+                </div> */}
 
-    <div>
-      {resList.map((item) => {
-        if(activeTab != item.tag){
-          return
-        }
-          return (
-            <div className="p-4 rounded-lg" id={item.tag} role="tabpanel">
-              <div className="text-md text-gray-500 dark:text-gray-400">
-                <Paragraphs text={item.text} />
-              </div>
+                <div className="flex flex-col mt-6 ">
+                    <div className="flex flex-row p-5 items-center justify-center gap-4">
+                      <img className="h-80 w-80 sm:w-[5rem] sm:h-[5rem] flex-shrink-0 object-cover rounded-full" src={item.image} alt="" />
+                      <Title title={item.title} fontSize="text-lg  sm:text-xl "/>
+                    </div>
+                    <Paragraphs text={item.text} />
+                    {item.btnLabel && (
+                    <div className="mt-6 sm:-mx-2">
+                        <a href="#" className="inline-flex items-center justify-center w-full px-4 text-sm py-2.5 overflow-hidden text-white transition-colors duration-300 bg-gray-900 rounded-lg shadow sm:w-auto sm:mx-2 hover:bg-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
+                            <GlobeAsiaAustraliaIcon  className="w-5 h-5 mx-2 fill-current" />
+                            <span className="mx-2">
+                                {item.btnLabel}
+                            </span>
+                        </a>
+                    </div>
+                    )}
+                </div>
             </div>
           )
         })}
+      </div>
     </div>
+
   </Section>
   );
 }
