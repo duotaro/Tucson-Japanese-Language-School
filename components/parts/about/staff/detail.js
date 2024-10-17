325e4e522c4bb9ef746272943c71e8f8af5ced33
duotaro <taro8815@gmail.com>
2024-10-17 11:34:09 +0900

    fixed staff page

--- components/parts/about/staff/detail.js
+++ components/parts/about/staff/detail.js
@@ -1,29 +1,79 @@
 // components/ProfileCard.js
 import React from 'react';
+import Paragraphs from '../../text/paragraphs';
+import Image from 'next/image';
 
 const ProfileCard = ({item}) => {
-   const { name, title, image, description } = item
+   const { name, role, image, text } = item
   return (
-      <div className="flex relative my-10  lg:max-w-sm mx-auto py-5 px-5">
-        <div className="rounded-lg overflow-hidden shadow-lg bg-white">
-            <div className="absolute -mt-20 w-full flex justify-center">
-                <div className="h-32 w-32">
-                <img
+    //   <div className="flex relative my-10 w-full mx-auto py-5 px-5">
+    //     <div className="rounded-lg overflow-hidden shadow-lg bg-white flex flex-col">
+    //         <div className="absolute -mt-20 w-full flex justify-center">
+    //             <div className="h-32 w-32">
+    //             <img
+    //                 src={image}
+    //                 className="rounded-full object-cover h-full w-full shadow-md"
+    //                 alt="Profile"
+    //             />
+    //             </div>
+    //         </div>
+    //         <div className="p-6 mt-16 w-full">
+    //             <h1 className="font-bold text-3xl text-center mb-1">{name}</h1>
+    //             <p className="text-blue-800 text-sm text-center font-semibold">
+    //             {role.map((r, index) => {
+    //                 return (
+    //                 <>
+    //                 {index == 0 && (
+    //                     <br />
+    //                 )}
+    //                 {index != 0 && (
+    //                     <> / </>
+    //                 )}
+    //                 {r.title}
+    //                 </>
+    //                 )
+    //             })}
+    //             </p>
+    //             <div className="w-full">
+    //               <Paragraphs text={text} />
+    //             </div>
+    //         </div>
+    //     </div>
+    //   </div>
+      <div data-popover="profile-info-popover"
+      class="min-w-[95%] max-w-[95%] whitespace-normal break-words rounded-lg border border-blue-gray-50 bg-white p-4 font-sans text-sm font-normal text-blue-gray-500 shadow-lg shadow-blue-gray-500/10 focus:outline-none">
+      <div class="flex items-center justify-center gap-4 mb-2">
+          <Image
                     src={image}
-                    className="rounded-full object-cover h-full w-full shadow-md"
+                    class="relative inline-block object-cover object-center w-24 h-24 rounded-full"
                     alt="Profile"
+                    width={40}
+                    height={40}
                 />
-                </div>
-            </div>
-            <div className="p-6 mt-16">
-                <h1 className="font-bold text-3xl text-center mb-1">{name}</h1>
-                <p className="text-gray-800 text-sm text-center">{title}</p>
-                <p className="text-center text-gray-600 text-base pt-3 font-normal">
-                {description}
-                </p>
-            </div>
+          <div>
+            <h2 className="font-bold text-2xl text-center">{name}</h2>
+            <p className="text-blue-800 text-sm text-center font-semibold">
+                {role.map((r, index) => {
+                    return (
+                    <>
+                    {index == 0 && (
+                        <br />
+                    )}
+                    {index != 0 && (
+                        <> / </>
+                    )}
+                    {r.title}
+                    </>
+                    )
+                })}
+            </p>
         </div>
       </div>
+      
+      <p class="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 min-w-[100%] p-3">
+        <Paragraphs text={text} />
+      </p>
+    </div>
   );
 };
 
--- pages/about/staff/index.js
+++ pages/about/staff/index.js
@@ -4,9 +4,11 @@ import React, { useContext } from 'react';
 import LocaleContext from "../../../components/context/localeContext";
 import { useLocale } from "../../../utils/locale";
 import ProfileCardList from "../../../components/parts/about/staff";
+import { getDatabase } from "../../../lib/notion";
+import saveImageIfNeeded from "../../../components/download";
 
 
-export default function StaffPage({  }) {
+export default function StaffPage({ staffList, roleList }) {
   const { locale } = useContext(LocaleContext);
   const { json, metaTitleExtension } = useLocale(locale)
   let lang = json.navigation
@@ -25,9 +27,33 @@ export default function StaffPage({  }) {
 
       <div className="">
         <div className="row">
-          <ProfileCardList />
+          <ProfileCardList staffList={staffList} roleList={roleList}/>
         </div>
       </div>
     </Layout>
   );
 }
+
+
+export const getStaticProps = async (context) => {
+
+  let roleList = await getDatabase("122a8c0ecf8c80059934c64693cc39ca")
+  // get about
+  let staffList = await getDatabase("9b85f554b3fc42dcb9d38f1ec87b168c")
+  let props = []
+  for(let staff of staffList){
+    props.push(staff.properties)
+  }
+
+  await saveImageIfNeeded(props, "staff")
+
+
+  return {
+    props: {
+      staffList: staffList,
+      roleList: roleList
+    },
+    revalidate: 1
+  };
+};
+
