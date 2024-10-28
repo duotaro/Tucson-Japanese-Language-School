e9a9c6e7b9d324df38d01f915ca34572914ced18
duotaro <taro8815@gmail.com>
2024-10-28 13:02:33 +0900

    fixed meta info

--- utils/locale.js
+++ utils/locale.js
@@ -4,6 +4,6 @@ import ja from "../const/locale/ja.json";
 export const useLocale = (locale) => {
   const json = locale === "en" ? en : ja;
   let lang = json.navigation
-  const metaTitleExtension = `- ${lang.meta_title}(${lang.sub_title})`
+  const metaTitleExtension = `${lang.meta_title}(${lang.sub_title})`
   return { locale, json, metaTitleExtension };
 }
