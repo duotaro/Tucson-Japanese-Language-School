e731a6e9425470fb6edbec0edc91be1870dd94bd
duotaro <taro8815@gmail.com>
2024-10-29 10:42:54 +0900

    release

--- components/layout.js
+++ components/layout.js
@@ -3,15 +3,13 @@ import Navbar from './navbar'
 import Footer from './footer'
 import Nav from './parts/nav/nav'
 import Breadcrumb from "./parts/nav/breadcrumb";
-import Sponsor from "./parts/sponsor";
+import { useRouter } from 'next/router';
 
 export default function Layout({ children, breadcrumb }) {
-  let sponsorList = []
+  
+  const router = useRouter();
+  const canonicalUrl = `${process.env.NEXT_PUBLIC_DOMAIN}${router.asPath}`;
 
-  // for(let item of sponsors){
-  //   let sponsor = new SponsorEntity(item)
-  //   sponsorList.push(sponsor)
-  // }
 
   return (
     <>
@@ -20,6 +18,11 @@ export default function Layout({ children, breadcrumb }) {
         <meta name="keywords" content="Japan, Japanese, School, Tucson, children, class, culture, kids, language, クラス, ツーソン, 子ども, 学校, 日本, 日本語" />
         <meta property="og:image"  contents="https://cdn-ak.f.st-hatena.com/images/fotolife/d/duo-taro100/20230501/20230501153944.jpg"></meta>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="canonical"
+          href={canonicalUrl}
+          key="canonical"
+        />
       </Head>
         <Navbar />
         <Nav />
