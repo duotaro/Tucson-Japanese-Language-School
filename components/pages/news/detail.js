941aa641ef6335a61c8972d987065228146343c7
duotaro <taro8815@gmail.com>
2025-07-13 23:14:25 +0900

    キャッシュ

--- components/pages/news/detail.js
+++ components/pages/news/detail.js
@@ -486,11 +486,11 @@ export default function NewsDetailPage({ newsItem, locale, newsId, pageMap, bloc
                       <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
                     </svg>
                   </button>
-                  <button className="p-2 text-gray-400 hover:text-red-500 transition-colors">
+                  {/* <button className="p-2 text-gray-400 hover:text-red-500 transition-colors">
                     <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                       <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
                     </svg>
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </div>
